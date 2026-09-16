265637d4f0a4a5d8b3f80ff84c98c46831cc8c12
ryanzinzx <ryansantosromagnoli@gmail.com>
2026-09-15 21:44:51 -0300

    .

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react"
+
 const ProductsPage = () => {
+
+    const API_URL = import.meta.env.VITE_API_URL
+    const [products, setProducts] = useState([])
+    const [joias, setJoias] = useState([])
+
+    useEffect(()=> {
+        fetch(`${API_URL}products/category/electronics`)
+        .then((results) => results.json())
+        .then((data) => setProducts(data))
+
+        fetch(`${API_URL}products/category/jewerlery`)
+            .then((results) => results.json())
+            .then((data) => setJoias(data))
+    }, [])
+
     return (
-        <h1>Página de Produtos</h1>
+        <section>
+            <h1>Página de Produtos</h1>
+            {products.map(produtoAtual => (
+                <div>
+                    <h2>{produtoAtual.title}</h2>
+                    <img src="{produtoAtual.image}"/>
+                </div>
+            ))}
+        </section>
     )
 }
 
